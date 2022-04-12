6b27011c53c062992e3665cdf9e412c046e77a93
Isak Styf <isak.styf@masarinconsulting.se>
2022-04-12 12:10:07 +0200

    validate device config on load instead of at client request

--- internal/pkg/infrastructure/repositories/database/database.go
+++ internal/pkg/infrastructure/repositories/database/database.go
@@ -14,9 +14,9 @@ type Datastore interface {
 	GetDeviceFromDevEUI(eui string) (Device, error)
 }
 
-type db struct {
+type database struct {
 	log     zerolog.Logger
-	devices [][]string
+	devices map[string]device
 }
 
 func SetUpNewDatabase(log zerolog.Logger, devicesFile io.Reader) (Datastore, error) {
@@ -25,42 +25,75 @@ func SetUpNewDatabase(log zerolog.Logger, devicesFile io.Reader) (Datastore, err
 
 	knownDevices, err := r.ReadAll()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read csv data from file: %s", err.Error())
 	}
 
-	return &db{
+	db := &database{
 		log:     log,
-		devices: knownDevices,
-	}, nil
+		devices: map[string]device{},
+	}
+
+	// Create a set of allowed environments from a slice of allowed envs so that
+	// we can validate and provide helpful diagnostics if config is wrong
+	allowedEnvironments := []string{"air", "ground", "water", "indoors"}
+	setOfAllowedEnvironments := map[string]bool{}
+	for _, env := range allowedEnvironments {
+		setOfAllowedEnvironments[env] = true
+	}
+
+	for idx, d := range knownDevices {
+		if idx == 0 {
+			// Skip the CSV header
+			continue
+		}
+
+		devEUI := d[0]
+
+		dev, ok := db.devices[devEUI]
+		if ok {
+			return nil, fmt.Errorf("duplicate devEUI %s found on line %d in devices config", devEUI, (idx + 1))
+		}
+
+		lat, err := strconv.ParseFloat(d[2], 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse latitude for device %s: %s", devEUI, err.Error())
+		}
+		lon, err := strconv.ParseFloat(d[3], 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse longitude for device %s: %s", devEUI, err.Error())
+		}
+
+		environment := d[4]
+		if !setOfAllowedEnvironments[environment] {
+			return nil, fmt.Errorf("bad environment specified for device %s on line %d in config (\"%s\" not in %v)", devEUI, (idx + 1), environment, allowedEnvironments)
+		}
+
+		types := strings.Split(d[5], ",")
+
+		dev = device{
+			Identity:    d[1],
+			Latitude:    lat,
+			Longitude:   lon,
+			Environment: environment,
+			Types:       types,
+		}
+
+		db.devices[devEUI] = dev
+	}
+
+	db.log.Info().Msgf("loaded %d devices from configuration file", len(db.devices))
+
+	return db, nil
 }
 
-func (db *db) GetDeviceFromDevEUI(eui string) (Device, error) {
-	for _, d := range db.devices {
-		if eui == d[0] {
-			lat, err := strconv.ParseFloat(d[2], 64)
-			if err != nil {
-				return nil, err
-			}
-			lon, err := strconv.ParseFloat(d[3], 64)
-			if err != nil {
-				return nil, err
-			}
+func (db *database) GetDeviceFromDevEUI(eui string) (Device, error) {
 
-			types := strings.Split(d[5], ",")
-
-			dev := device{
-				Identity:  d[1],
-				Latitude:  lat,
-				Longitude: lon,
-				Where:     d[4],
-				Types:     types,
-			}
-
-			return dev, nil
-		}
+	device, ok := db.devices[eui]
+	if !ok {
+		return nil, fmt.Errorf("no matching devices found with devEUI %s", eui)
 	}
 
-	return nil, fmt.Errorf("no matching devices found with devEUI %s", eui)
+	return device, nil
 }
 
 type Device interface {
@@ -68,11 +101,11 @@ type Device interface {
 }
 
 type device struct {
-	Identity  string   `json:"id"`
-	Latitude  float64  `json:"latitude"`
-	Longitude float64  `json:"longitude"`
-	Where     string   `json:"where"`
-	Types     []string `json:"types"`
+	Identity    string   `json:"id"`
+	Latitude    float64  `json:"latitude"`
+	Longitude   float64  `json:"longitude"`
+	Environment string   `json:"environment"`
+	Types       []string `json:"types"`
 }
 
 func (d device) ID() string {
---
+++ internal/pkg/infrastructure/repositories/database/database_test.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/matryer/is"
+	"github.com/rs/zerolog"
+)
+
+func TestThatLoadFailsOnDuplicateDeviceEUI(t *testing.T) {
+	is := is.New(t)
+	_, err := SetUpNewDatabase(zerolog.Logger{}, bytes.NewBuffer([]byte(csvWithDuplicates)))
+	is.True(err != nil)
+}
+
+func TestThatLoadFailsOnBadLatitude(t *testing.T) {
+	is := is.New(t)
+	_, err := SetUpNewDatabase(zerolog.Logger{}, bytes.NewBuffer([]byte(csvWithBadLatitude)))
+	is.True(err != nil)
+}
+
+func TestThatLoadFailsOnBadLongitude(t *testing.T) {
+	is := is.New(t)
+	_, err := SetUpNewDatabase(zerolog.Logger{}, bytes.NewBuffer([]byte(csvWithBadLongitude)))
+	is.True(err != nil)
+}
+
+func TestThatLoadFailsOnBadEnvironment(t *testing.T) {
+	is := is.New(t)
+	_, err := SetUpNewDatabase(zerolog.Logger{}, bytes.NewBuffer([]byte(csvWithBadEnvironment)))
+	is.True(err != nil)
+}
+
+const csvWithDuplicates string = `devEUI;internalID;lat;lon;where;types
+a81758fffe051d00;intern-a81758fffe051d00;0.0;0.0;air;urn:oma:lwm2m:ext:3303
+a81758fffe051d00;intern-a81758fffe04d83f;0.0;0.0;ground;urn:oma:lwm2m:ext:3303`
+
+const csvWithBadLatitude string = `devEUI;internalID;lat;lon;where;types
+a81758fffe051d00;intern-a81758fffe051d00;gurka;0.0;air;urn:oma:lwm2m:ext:3303`
+
+const csvWithBadLongitude string = `devEUI;internalID;lat;lon;where;types
+a81758fffe051d00;intern-a81758fffe051d00;0.0;gurka;air;urn:oma:lwm2m:ext:3303`
+
+const csvWithBadEnvironment string = `devEUI;internalID;lat;lon;where;types
+a81758fffe051d00;intern-a81758fffe051d00;0.0;0.0;gurka;urn:oma:lwm2m:ext:3303`
